a0bb6633971c1f7865a8f8c81ef874054dba579f
Brendon-Egolf <begolf123@gmail.com>
2016-10-06 22:08:35 -0400

    Finished project 2

--- project-2/make-sandbox.sh
+++ project-2/make-sandbox.sh
@@ -4,7 +4,8 @@ mkdir -p sandbox/css
 mkdir -p sandbox/images
 mkdir -p sandbox/scripts
 touch sandbox/index.html
-cp /var/www/images/logo?.png sandbox
+cp sandbox/var/www/images/logo1.png sandbox
+cp sandbox/var/www/images/logo2.png sandbox
 chmod -R 444 sandbox
 ls -aR sandbox > web-listing
-ls /var/www/images | grep -P '\.png' > avail-images
+ls sandbox/var/www/images | grep -P '\.png' > avail-images
